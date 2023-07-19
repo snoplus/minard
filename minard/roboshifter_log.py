66451df965f75c5b1cdf7aed59211499c6f260bc
David Drobner <david@drobner.ca>
2023-07-19 14:26:53 -0400

    Implement function to read current roboshifter logfile

---
+++ minard/roboshifter_log.py
@@ -0,0 +1,9 @@
+# TODO replace this with WHERE the log will actually be stored once deployed
+# alternatively add to some type of config somewhere, don't think this is how it's done in minard through
+
+def get_roboshifter_log():
+    log = ""
+    with open("/home/david/alarmgui/logs/roboshifter.log", "r") as f:
+        for line in reversed(f):
+            log += line
+    return log
